c9e957f0c8c11cde6f1128819904ead7b8875657
Nikhil-chhajer <nikhil.chhajer80@gmail.com>
2023-11-01 20:08:36 +0530

    added city-controller

--- src/controller/city-controller.js
+++ src/controller/city-controller.js
@@ -1,1 +1,83 @@
-const CityService=require('../services/index.js');
+const {CityService}=require('../services/index.js');
+const cityService=new CityService();
+
+const create=async (res,req)=>{
+     try {
+        const city=await cityService.createCity(req.body)
+        return res.status(201).json({
+            data :city,
+            success:true,
+            messgage:'Sucessfully created a city',
+            err:{}
+        });
+     } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data:{},
+            success:false,
+            message:'Not able to create a city',
+            err:error
+        })
+     }
+}
+//DELETE -> /city/:id
+const destroy=async(res,req)=>{
+    try {
+        const response=await cityService.deleteCity(req.params.id)
+        return res.status(200).json({
+            data :response,
+            success:true,
+            messgage:'Sucessfully deleted a city',
+            err:{}
+        });
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data:{},
+            success:false,
+            message:'Not able to delete a city',
+            err:error
+        })
+    }
+}
+//GET. -> /city/:id
+const get=async(res,req)=>{
+    try {
+        const response=await cityService.getCity(req.params.id)
+        return res.status(200).json({
+            data :response,
+            success:true,
+            messgage:'Sucessfully fetched a city',
+            err:{}
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data:{},
+            success:false,
+            message:'Not able to get a city',
+            err:error
+        })
+    }
+}
+//PATCH -> /city/:id ->req.body
+const update=async(res,req)=>{
+    try {
+        const response=await cityService.updateCity(req.params.id,req.body)
+        return res.status(200).json({
+            data :response,
+            success:true,
+            messgage:'Sucessfully updated a city',
+            err:{}
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data:{},
+            success:false,
+            message:'Not able to update a city',
+            err:error
+        })
+    }
+}
